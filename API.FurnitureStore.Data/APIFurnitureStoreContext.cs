12472ec57b2b6d2c571236e1d44f253f1967c533
ezequiel-piki <ezequielpiki23@gmail.com>
2023-04-08 15:52:24 -0300

    Agregar archivos de proyecto.

---
+++ API.FurnitureStore.Data/APIFurnitureStoreContext.cs
@@ -0,0 +1,28 @@
+﻿using API.FurnitureStore.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.FurnitureStore.Data
+{
+    public class APIFurnitureStoreContext : DbContext
+    {
+        public APIFurnitureStoreContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+
+        public DbSet <Client> Clients { get; set; }
+        public DbSet <Product> Products { get; set; }
+        public DbSet <Order> Orders { get; set; }
+        public DbSet <ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
