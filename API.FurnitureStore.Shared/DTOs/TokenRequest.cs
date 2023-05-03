54dd8a49cbcd23ff2d425cb2f8f59289961db9fe
ezequiel-piki <ezequielpiki23@gmail.com>
2023-05-03 20:48:37 -0300

    Clase RefreshToken creada, TokenRequest.cs update

--- API.FurnitureStore.Shared/DTOs/TokenRequest.cs
+++ API.FurnitureStore.Shared/DTOs/TokenRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace API.FurnitureStore.Shared.DTOs
 {
     public class TokenRequest
     {
+        [Required]
         public string Token { get; set; }
+
+        [Required] 
         public string RefreshToken { get; set; }
     }
 }
