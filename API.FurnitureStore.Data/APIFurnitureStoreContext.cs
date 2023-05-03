f5bd7eccad01bcf66bb299ca6d7f72b411b4f8a1
ezequiel-piki <ezequielpiki23@gmail.com>
2023-05-03 20:51:12 -0300

    DBSet<RefreshToken> añadido a APIFurnitureStoreContext.cs

--- API.FurnitureStore.Data/APIFurnitureStoreContext.cs
+++ API.FurnitureStore.Data/APIFurnitureStoreContext.cs
@@ -20,8 +20,8 @@ namespace API.FurnitureStore.Data
         public DbSet <Product> Products { get; set; }
         public DbSet <Order> Orders { get; set; }
         public DbSet <ProductCategory> ProductCategories { get; set; }
-
         public DbSet <OrderDetail> OrderDetails { get; set; }
+        public DbSet <RefreshToken> RefreshTokens { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
